defae5614fc4ceb26e9dc3239b712a69c95f718b
Pradiksh Soman <Pradiksh99@gmail.com>
2026-03-19 07:51:16 +0530

    updated code

--- src/Arrays/MaxElementArray.java
+++ src/Arrays/MaxElementArray.java
@@ -1,4 +1,25 @@
 package Arrays;
+//Given an integer array nums, return the maximum element in the array.
+//Input: nums = [3, 7, 2, 9, 4]
+//Output: 9
 
+//Input: nums = [-10, -3, -5, -2]
+//Output: -2
 public class MaxElementArray {
+
+public int findMaxElement(int[] nums) {
+    int max = Integer.MIN_VALUE;
+    for(int i = 0; i<nums.length; i++){
+        if(nums[i] > max){
+            max = nums[i];
+        }
+    }
+    return max;
+};
+    public static void main(String[] args) {
+int[] arr = {-1,-2,-44,-5,-6,-12};
+        MaxElementArray MaxElem = new MaxElementArray();
+        System.out.println(MaxElem.findMaxElement(arr));
+    }
 }
+
--- src/Arrays/ReverseArray.java
+++ src/Arrays/ReverseArray.java
@@ -1,4 +1,22 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class ReverseArray {
+public int[] Reversed(int[] arr){
+    int[] Revarray = new int[arr.length];
+
+    for(int j = arr.length - 1, i = 0; j >= 0; j--, i++ ){
+        Revarray[i] = arr[j];
+    }
+return Revarray;
+}
+
+    public static void main(String[] args) {
+
+int[] array = {1,2,3,4,5,6};
+        ReverseArray rev = new ReverseArray();
+
+        System.out.print(Arrays.toString(rev.Reversed(array)));
+    }
 }
