e461da96b489afd63e3cbcf2120b9e309c83c4da
Pradiksh Soman <Pradiksh99@gmail.com>
2026-03-23 18:10:54 +0530

    This contains code to move zeroes

---
+++ src/Arrays/MoveZeroes.java
@@ -0,0 +1,50 @@
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+//Note that you must do this in-place without making a copy of the array.
+//Example 1:
+//
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+//Example 2:
+//
+//Input: nums = [0]
+//Output: [0]
+//
+//
+//Constraints:
+//
+//        1 <= nums.length <= 104
+//        -231 <= nums[i] <= 231 - 1
+//
+//
+//Follow up: Could you minimize the total number of operations done?
+package Arrays;
+
+import java.util.Arrays;
+public class MoveZeroes {
+
+
+    public int[] MoveZeroesRight(int[]nums){
+        int n = nums.length;
+        int left = 0;
+        for(int right = 0; right<n;right++){
+            if(nums[right]!=0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+        }
+return nums;
+    }
+
+    public static void main(String[]args){
+        int[]arr = {0,1,0,3,12};
+        MoveZeroes ob1 = new MoveZeroes();
+
+        ob1.MoveZeroesRight(arr);
+        System.out.print(Arrays.toString(arr));
+    }
+
+
+}
