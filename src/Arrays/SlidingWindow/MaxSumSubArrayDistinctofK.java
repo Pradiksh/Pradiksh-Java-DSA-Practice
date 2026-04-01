6cf69f45281d674b83297cb577a23daa3eaa4204
Pradiksh Soman <Pradiksh99@gmail.com>
2026-04-01 17:43:16 +0530

    This commit hase longest substring code

--- src/Arrays/SlidingWindow/MaxSumSubArrayDistinctofK.java
+++ src/Arrays/SlidingWindow/MaxSumSubArrayDistinctofK.java
@@ -1,4 +1,18 @@
 package Arrays.SlidingWindow;
 
 public class MaxSumSubArrayDistinctofK {
+
+
+    static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,6,8,9,12,14,16,19};
+
+        for(int i =0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println("Duplicate found");
+                }
+            }
+        }
+
+    }
 }
